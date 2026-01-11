4c731ff6798f96df052a16c7561b83fdbbf6cec6
sanjana-raghu <sanjanaraghu252002@gmail.com>
2026-01-12 00:32:40 +0530

    Create List_methods.py

---
+++ List_methods.py
@@ -0,0 +1,16 @@
+#2.(b) Write a python program to implement List methods (Add, Append, and Extend& Delete). 
+fruits=['Apple','Banana','Cherry','Grapes','Orange'] 
+print(fruits) 
+ 
+fruits.insert(2,'watermelon') 
+print(fruits) 
+ 
+#Add function 
+fruits.append('mango') 
+print(fruits) 
+ 
+fruits.extend([4,5,6]) 
+print(fruits) 
+ 
+fruits.remove('Banana') 
+print(fruits)
