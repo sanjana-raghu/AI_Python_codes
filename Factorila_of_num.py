19d3f3a05433e5d47d8287599be4e8ad6a2a586f
sanjana-raghu <sanjanaraghu252002@gmail.com>
2026-01-11 20:53:11 +0530

    Create Factorila_of_num.py

---
+++ Factorila_of_num.py
@@ -0,0 +1,6 @@
+#1.(C)Write a python program to find factorial of the given number? 
+num=int(input('enter the number ::')) #num=5 
+fact=1 
+for i in range(1,num+1):  
+    fact=fact*i 
+print("The Factorial of 5 is:",fact) 
